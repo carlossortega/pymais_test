fec0debcf5fafaa6b1ea1a8cd2a0a9101cc1e084
carlossortega <carlosivanom@gmail.com>
2024-10-10 12:06:18 -0600

    Ajuste de Youtube Video url

--- resources/views/admin/course/youtube_type_lesson_add.blade.php
+++ resources/views/admin/course/youtube_type_lesson_add.blade.php
@@ -3,12 +3,12 @@
 
 <div class="form-group mb-2">
     <label class="form-label ol-form-label">{{ get_phrase('Video url') }}</label>
-    <input type="text" id="video_url" onblur="ajax_get_video_details(this.value)" name="lesson_src" class="form-control ol-form-control">
+    <input type="text" id="video_url" {{-- onblur="ajax_get_video_details(this.value)" --}} name="lesson_src" class="form-control ol-form-control">
     <small class="form-label text-danger text-12px d-hidden mb-0" id="invalid_url">{{ get_phrase('Invalid url') }}. {{ get_phrase('Your video source has to be either YouTube') }}</small>
 </div>
 
 <div class="form-group mb-2">
     <label class="form-label ol-form-label">{{ get_phrase('Duration') }}</label>
     <small class="form-label text-12px d-hidden mb-0" id="perloader"><i class="fi-rr-loading mdi-loading "></i> {{get_phrase('Analyzing')}}....</small>
-    <input type="text" name="duration" id="duration" class="form-control ol-form-control" value="00:00:00" readonly>
+    <input type="text" name="duration" id="duration" class="form-control ol-form-control" value={{ old('duration') }}">
 </div>
